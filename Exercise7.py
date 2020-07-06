5214937d9e73e5d787a7fc30698ad722c7c181c9
alberto <albertogarciaprieto13@gmail.com>
2020-07-06 03:55:35 +0200

    add exercise7

---
+++ Exercise7.py
@@ -0,0 +1,9 @@
+import urllib.request
+
+fp = urllib.request.urlopen("https://www.google.com")
+mybytes = fp.read()
+
+mystr = mybytes.decode("utf8")
+fp.close()
+print("La cantidad de veces que esta en la pagina https://www.fhios.es la palabra fhios es: {}".format(mystr.find("fhios")))
+#print(myst
