6ccbe4b6e9d3ba3840867478bb809eec24c590a1
alberto <albertogarciaprieto13@gmail.com>
2020-07-09 12:14:16 +0200

    modify exercise 7

--- Exercise7.py
+++ Exercise7.py
@@ -1,9 +1,39 @@
+import queue
 import urllib.request
+import re
+from urllib.parse import urljoin
 
-fp = urllib.request.urlopen("https://www.fhios.es")
-mybytes = fp.read()
+cont = 0
 
-mystr = mybytes.decode("utf8")
-fp.close()
-print("La cantidad de veces que esta en la pagina https://www.fhios.es la palabra fhios es: {}".format(mystr.find("fhios")))
-#print(myst
+def descargar(pagina,cont):
+    if pagina != "https://www.fhios.es/blog/" :
+        try:
+            peticion = urllib.request.Request(pagina)
+            html = urllib.request.urlopen(peticion).read()
+            print("[*] Descarga OK >>", pagina)
+            html1 = html.decode("utf8")
+            print(html1.count("fhios"))
+            cont = cont + html1.count("fhios")
+        except:
+            return None, cont
+    else:
+        return None, cont
+
+    return html, cont
+
+
+def rastrearEnlaces(pagina,cont):
+    visitados = [pagina]
+    buscaEnlaces = re.compile('<a[^>]+href=["\'](.*?)["\']', re.IGNORECASE)
+    print("Buscando enlaces en",pagina)
+    html = descargar(pagina,cont)
+    enlaces = buscaEnlaces.findall(str(html[0]))
+    for enlace in enlaces:
+        if enlace not in visitados :
+            html = descargar(enlace,html[1])
+            visitados.append(enlace)
+    return html[1]
+
+
+cont = 0
+print("Numero total", rastrearEnlaces("https://www.fhios.es",cont))
